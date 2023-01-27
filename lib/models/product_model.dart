fc6af554b39b7b35790b6474bf30bb535d052975
kolloshe <mohamedmhadi287@gmail.com>
2023-01-27 06:56:14 -0800

    test 2

---
+++ lib/models/product_model.dart
@@ -0,0 +1,10 @@
+class Product {
+ 
+ final String productName;
+ final String productPrice;
+ final String productDescription;
+ final String productImage;
+
+ Product(this.productName,this.productDescription,this.productImage,this.productPrice);
+
+}
